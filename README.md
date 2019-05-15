5781efe730fa5e53cbbfd14e573d69d49024c1fc
Guilherme Ramos <gui_aramos@outlook.com>
2019-05-15 11:42:20 +0900

    Final project with the system working properly

--- README.md
+++ README.md
@@ -1,27 +1,26 @@
-# Credit Card Validation
+# Vigenère’s cipher
 
-This is a simple program to transform a plaintext to ciphertext using Caesar cryptosystem written in C
+A program called vigenere that enables you to encrypt messages using Vigenère’s cipher written in C
 [Pseuodocode](pseuodocode.txt)
 
 ## Task
 
 Create a program that transforms the plaintext to ciphertext based on the argument provided by the user
-[CS50 2019 - Caesar](https://lab.cs50.io/cs50/labs/2019/x/caesar/)
+[CS50 2019 - Vigenere](https://lab.cs50.io/cs50/labs/2019/x/vigenere/)
 
 ## Criteria
 
-The program must receive a non-negative argument and ignore non-Alphabet char
-
+The program must receive a word argument and ignore non-Alphabet char
 
 ## Functionality
 
-This program asks for the argument and applies Caesar cryptosystem on a plaintext, thus generating a ciphertext
+This program asks for the argument and applies Vigenere cryptosystem on a plaintext, thus generating a ciphertext
 
 ## Run Locally
 
-Run this command git clone https://github.com/guiaramos/caesar-cryptosystem.git.
+Run this command git clone https://github.com/guiaramos/vigenere-cryptosystem.git.
 Make sure your PC is able to compile Clang
-Run "make Caesar" and then "./caesar"
+Run "make vigenere" and then "./vigenere"
 
 ## Tech Stack
 
--- vigenere.c
+++ vigenere.c
@@ -3,27 +3,24 @@
 #include <string.h>
 #include <ctype.h>
 
-// This a program for encrypt any text with caesar sytem.
-
-/* Usage:
-* When runing the user needs to inform a key to increment the plaintext
-* after that, the user needs to inform the plain text to be encrypted
-*/
+int shift(char c); // this functions convert the char into int
 
 int main(int argc, char const *argv[])
 {
   // Check if the argument was informed and not a negative
-  if (argc != 2 || argc < 0)
+  int isInt = atoi(argv[1]); // assigning the key to a variable int for checking
+
+  if (argc != 2 || argc < 0 || isdigit(isInt))
   {
-    printf("Usage: ./caesar key\n");
+    printf("Usage: ./vigenere keyword\n");
     return 1;
   }
 
   // Ask for the plaintext to be encrypted
   string plaintext = get_string("plaintext: ");
   int n = strlen(plaintext); // getting the length of the plaintext
-
-  int k = atoi(argv[1]); // assigning the key to a variable int
+  int lenK = strlen(argv[1]); // getting the length of the keywork
+  int j = 0; // assigning for looping the keyword length
 
   printf("ciphertext: ");
 
@@ -34,6 +31,14 @@ int main(int argc, char const *argv[])
 
     if (isalpha(plaintext[i])) // first check if Alphabet
     {
+      if (j == lenK) // reseting the keyword char
+      {
+        j = 0;
+      }
+
+      int k = shift(argv[1][j]); // transform the keyword into int
+      j++; // junping to the next keyword
+
       if (sNode)
       {
         x = ((((plaintext[i] + k)- 97) % 26 ) +97);
@@ -47,4 +52,20 @@ int main(int argc, char const *argv[])
   }
   printf("\n"); // just formating for another line
 
+}
+
+int shift (char c)
+{
+  int s = 0;
+  bool ctype = islower(c); // assigning to check the char type
+
+  if (ctype)
+  {
+    s = (c - 97);
+  }
+  else
+  {
+    s = (c - 65);
+  }
+ return s;
 }
